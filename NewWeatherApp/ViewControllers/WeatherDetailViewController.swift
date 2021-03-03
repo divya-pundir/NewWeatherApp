fce013c143fefe39be06ec91abc91fd337ed817f
Divya <divya27rana@gmail.com>
2021-03-03 15:28:33 +0530

    Add navigation title

--- NewWeatherApp/ViewControllers/WeatherDetailViewController.swift
+++ NewWeatherApp/ViewControllers/WeatherDetailViewController.swift
@@ -14,6 +14,7 @@ class WeatherDetailViewController : UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = selectedCity?.name
     }
     
     override func viewWillAppear(_ animated: Bool) {
